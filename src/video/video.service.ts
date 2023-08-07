a408713e0958d250d531cdcd79ca850237d4165d
abdulmtahir <taheer2005@yahoo.com>
2023-08-07 10:33:54 +0100

    videos

--- src/video/video.service.ts
+++ src/video/video.service.ts
@@ -1,26 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Video } from './entities/video.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class VideoService {
-  create(createVideoDto: CreateVideoDto) {
-    return 'This action adds a new video';
+  constructor(
+    @InjectRepository(Video)
+    private readonly videoRepo: Repository<Video>
+  ){}
+
+  create(createVideoDto: CreateVideoDto): Promise<Video> {
+    return this.videoRepo.save(createVideoDto);
   }
 
-  findAll() {
-    return `This action returns all video`;
+  findAll(): Promise<Video[]> {
+    const videos =  this.videoRepo.find();
+    try {
+      return videos;
+  } catch (error) {
+      throw new NotFoundException('The videos list is empty for now.');
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} video`;
   }
 
-  update(id: number, updateVideoDto: UpdateVideoDto) {
-    return `This action updates a #${id} video`;
+  findOne(id: number): Promise<Video> {
+    const video = this.videoRepo.findOne({where:{id}});
+    try {
+      return video;
+  } catch (error) {
+      throw new NotFoundException('The video is not found.');
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} video`;
+  }
+
+  async update(id: number, updateVideoDto: UpdateVideoDto): Promise<Video> {
+    //return this.videoRepo.update(id,updateCampaignDto);
+    const updateVideo = await this.videoRepo.findOne({ where: {id}});
+    if (!updateVideo) {
+      throw new Error('The video is not found.');
+    }
+
+    // Update the fields from the DTO
+    Object.assign(updateVideo, updateVideoDto);
+    return this.videoRepo.save(updateVideo);
+  }
+
+  remove(id: number){
+  return  this.videoRepo.delete(id);
   }
 }
