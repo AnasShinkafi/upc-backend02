b6f33e5383067da891951e2669f0684681b8fdaa
Anas <anasisahskf59@gmail.com>
2023-08-08 09:21:55 +0100

    register

--- src/register-admin/dto/update-register-admin.dto.ts
+++ src/register-admin/dto/update-register-admin.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { registerAdminDto } from './create-register-admin.dto';
+import { CreateRegisterAdminDto } from './create-register-admin.dto';
 
-export class UpdateRegisterAdminDto extends PartialType(registerAdminDto) {}
+export class UpdateRegisterAdminDto extends PartialType(CreateRegisterAdminDto) {}
