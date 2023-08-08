5b2b43e276933627fe7b05af5e300ea7205f8455
Anas <anasisahskf59@gmail.com>
2023-08-08 09:46:38 +0100

    gallery

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GalleriesModule } from './galleries/galleries.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       }), 
       inject: [ConfigService],
     }),
+    GalleriesModule,
      
   ],
   controllers: [AppController],
