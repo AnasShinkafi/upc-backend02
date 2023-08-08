ffc81a1bc3c65c907a4465b435a85df30d68198c
Anas <anasisahskf59@gmail.com>
2023-08-08 11:07:02 +0100

    membership

--- src/membership/dto/create-membership.dto.ts
+++ src/membership/dto/create-membership.dto.ts
@@ -1,1 +1,39 @@
-export class CreateMembershipDto {}
+import { IsEmail, IsNumber, IsString } from "class-validator";
+import { Gender } from "../gneder.enum";
+
+export class CreateMembershipDto {
+
+    @IsString()
+    first_name: string;
+
+    @IsString()
+    last_name: string;
+
+    @IsString()
+    picture: string;
+
+    @IsString()
+    age: Gender[];
+
+    @IsString()
+    @IsEmail()
+    email: string;
+
+    @IsNumber()
+    phone_number: number;
+
+    @IsString()
+    address_1: string;
+
+    @IsString()
+    address_2: string;
+
+    @IsString()
+    postal_code: string;
+
+    @IsString()
+    city: string;
+
+    @IsString()
+    state: string;
+}
