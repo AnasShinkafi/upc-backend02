33da390d42f1ff5f1d5b64105ea5154ff0a2a16b
Anas <anasisahskf59@gmail.com>
2023-08-08 09:34:10 +0100

    news-latter

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { NewsLatterModule } from './news-latter/news-latter.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       }), 
       inject: [ConfigService],
     }),
+    NewsLatterModule,
      
   ],
   controllers: [AppController],
